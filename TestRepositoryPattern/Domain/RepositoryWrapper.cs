fae9c53c1e04d3c6dcbc535791837d497694cd7d
VENKATESH MANI <venkatesh.mani@ucsf.edu>
2020-10-06 15:55:54 +0530

    Remove unwanted namespaces

--- TestRepositoryPattern/Domain/RepositoryWrapper.cs
+++ TestRepositoryPattern/Domain/RepositoryWrapper.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace TestRepositoryPattern.Domain
+﻿namespace TestRepositoryPattern.Domain
 {
      public class RepositoryWrapper : IRepositoryWrapper
      {
